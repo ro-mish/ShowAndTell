24c1c0fa4441583044d17e08c1f91132de6eda82
Rohit Mishra <rohitnmishra2@gmail.com>
2023-08-30 15:44:33 -0500

    updated w gpt

--- app.py
+++ app.py
@@ -15,157 +15,11 @@ from PIL import Image, ImageDraw, ImageEnhance
 from sklearn.cluster import KMeans
 from torchvision import datasets, transforms
 
+from src.utils import *
 
-def upload_image():
-    """
-    Function to upload an image using Streamlit's file_uploader.
-    
-    Returns:
-        PIL.Image or None: A PIL Image if an image is uploaded, otherwise None.
-    """
-    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
-    
-    if uploaded_file is not None:
-        image = Image.open(uploaded_file)
-        #st.image(image, caption='Uploaded Image.', use_column_width=True)
-        st.write("Image successfully uploaded!")
-        return image
-    
-    return None
+os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
 
-def load_image(img, transform=None):
-    #img = Image.open(image_path).convert('RGB')
-    if transform is not None:
-        img = transform(img)
-    st.image(image)
-    # Add the batch dimension
-    img = img.unsqueeze(0)
-    return img
-
-@st.cache_resource
-def load_model(path):
-    # new_model = timm.create_model('vit_base_patch16_224', pretrained=True, num_classes=7).to("cpu")
-    # # # Load the weights
-    
-    # # new_model.eval()  # Set the model to evaluation mode
-    
-    # #url = "https://drive.google.com/uc?export=download&id=1Ia58B9ynHYYIQByeB5lSoqZi6RumjA6T"
-    # url = "https://drive.google.com/uc?export=download&id=19TsWdOq6a2lRkLoBuaJGw_8zRhL7cGox"
-    
-    # # output = 'emotion_model.pkl'
-    # gdown.download(url, "emotion_weights.pth", quiet=False)
-    # new_model.load_state_dict(torch.load("emotion_weights.pth", map_location=torch.device('cpu')))
-    # # with open('emotion_model.pkl', 'rb') as f:
-    # #     new_model = pickle.load(f)
-    
-    # #file_url = 'https://drive.google.com/uc?export=download&id=YOUR_FILE_ID'  # Replace with your actual link
-
-    # # # Create an in-memory binary stream
-    # # buffer = io.BytesIO()
-
-    # # # Download file into the buffer
-    # # gdown.download(url, output=buffer, quiet=False)
-
-    # # # Reset buffer position to the beginning
-    # # buffer.seek(0)
-
-    # # # Load the pickled data from the buffer
-    # # new_model = pickle.load(buffer)
-    # new_model.eval()
-    
-    # 1. Create the model architecture
-    new_model = timm.create_model('vit_base_patch16_224', pretrained=True, num_classes=7).to("cpu")
-
-    # 2. Download the weights into an in-memory buffer
-    url = "https://drive.google.com/uc?export=download&id=19TsWdOq6a2lRkLoBuaJGw_8zRhL7cGox"
-    buffer = io.BytesIO()
-    gdown.download(url, output=buffer, quiet=False)
-
-    # 3. Load the model weights from this buffer
-    buffer.seek(0)
-    new_model.load_state_dict(torch.load(buffer, map_location=torch.device('cpu')))
-
-    # 4. Set the model to evaluation mode
-    new_model.eval()
-    return new_model
-
-
-def get_top_colors(img, factor=1.1, num=5):
-    """
-    Returns the top `num` colors in the image at `img_path`.
-    """
-    # # Load image and convert to RGB
-    # img = Image.open(img_path).convert('RGB')
-    img = img.convert('RGB')
-    enhancer = ImageEnhance.Contrast(img)
-    img = enhancer.enhance(factor)
-    # Convert image data to list of tuples
-    data = list(img.getdata())
-    
-    # Count occurrences of each color
-    color_count = Counter(data)
-    
-    # Get the most common colors
-    top_colors = [item[0] for item in color_count.most_common(num)]
-    st.write(top_colors)
-    return top_colors
-
-def create_color_squares(top_colors, square_size=100):
-    """
-    Returns an image with concatenated squares for each color in `top_colors`.
-    """
-    width = square_size * len(top_colors)
-    height = square_size
-    
-    # Create a new blank image with the computed width and height
-    img = Image.new('RGB', (width, height))
-    
-    draw = ImageDraw.Draw(img)
-    
-    for idx, color in enumerate(top_colors):
-        draw.rectangle(
-            [idx * square_size, 0, (idx + 1) * square_size, height],
-            fill=color
-        )
-    
-    return img
-
-def get_unique_colors(img, k=5):
-    """
-    Use k-means clustering to find the k most "unique" colors in the image.
-    """
-    # Load the image
-    
-    
-    # Convert the image to an array of RGB values
-    img_data = np.array(img)
-    pixels = img_data.reshape(-1, 3)
-    
-    # Use k-means clustering to find k unique colors
-    kmeans = KMeans(n_clusters=k)
-    kmeans.fit(pixels)
-    
-    # Extract the cluster centers
-    unique_colors = kmeans.cluster_centers_
-    
-    # Convert to integers
-    unique_colors = unique_colors.round(0).astype(int)
-    
-    return unique_colors.tolist()
-
-def visualize_colors(colors):
-    """
-    Create an image displaying the unique colors side by side.
-    """
-    color_count = len(colors)
-    img = Image.new('RGB', (color_count * 100, 100))
-    draw = ImageDraw.Draw(img)
-    
-    for idx, color in enumerate(colors):
-        draw.rectangle([idx * 100, 0, (idx + 1) * 100, 100], fill=tuple(color))
-    
-    return img
-
+openai.api_key = os.environ['OPENAI_API_KEY']
 
 id_2_label = {
         0: 'Angry',
@@ -178,15 +32,22 @@ id_2_label = {
     }
 
 
-col1, col2 = st.columns(2)
+col1, col2 = st.columns([2,1], gap="large")
 
+im_check = False
+    
 if __name__ == "__main__":
     
     
-    st.title("Show and Tell")
     
-        
-    st.header("Upload a picture of a face to predict the person's emotion")
+
+    st.title("Show & Tell")
+    
+    st.caption("A tool to generate a set design based on the emotion & colors present in an image of person. \
+        Get started by uploading a \
+        Your data is never persisted anywhere.")
+    
+    st.header("Upload an image to get started")
     image = upload_image()
     #st.lottie("https://lottie.host/86a4be01-7274-4cdc-878c-1040815eb450/sowTE5AlrP.json")
     #st.lottie("https://lottie.host/a33a008b-43d1-478b-ae0f-5e81a9fef8ff/7QxIUNpemB.json")
@@ -200,23 +61,38 @@ if __name__ == "__main__":
     ])
     
     PATH='model.pkl'
-    
-    model = load_model(PATH)
-    
-    st.subheader("Predicted emotion")
+    with st.spinner("Getting the model from the pit of the Mariana Trench..."):
+        model = load_model(PATH)
     
     if image:
+        im_check = True
         
         tc = get_unique_colors(image)
         
         image = load_image(image, transform=transform)
+        with st.spinner("Getting the model from the pit of the Mariana Trench..."):
         
-        outputs = model(image)
+            outputs = model(image)
+            outputs = outputs.detach().numpy().flatten()
+            
+        st.write(f"Predicted emotion from image: {id_2_label[np.argmax(outputs)]}")
+        options = ["Christopher Nolan", "Quentin Tarantino", "Steven Spielberg", "Martin Scorsese"]
+        selected_option = None
         
-        st.subheader(f"Predicted emotion from image: {id_2_label[torch.argmax(outputs).item()]}")
+        st.header("Select a director to generate a set design")
+        selected_option = st.selectbox("Select a director", options)
+
+        with st.expander("Show model details and colors", expanded=False):
+            fig = px.bar(x=list(id_2_label.values()), y=outputs, labels={'x':'Emotion Category', 'y': 'Activation'})
+            st.plotly_chart(fig)
+            
+            st.write("Top colors in the image:")
+            st.image(visualize_colors(tc))     
         
-        fig = px.bar(x=list(id_2_label.values()), y=outputs.detach().numpy().flatten(), labels={'x':'Emotion Category', 'y': 'Probability'})
-        st.plotly_chart(fig)
         
-        st.write("Top colors in the image:")
-        st.image(visualize_colors(tc))
+        with st.expander("Generated set description", expanded=False):
+            with st.spinner("Taking this info to the writer's room..."):
+                st.write(write_screenplay(emotion=id_2_label[np.argmax(outputs)], colors=tc, director=selected_option)["choices"][0]["message"]["content"])
+    
+
+           
